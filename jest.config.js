b0cc3b316af2e413666b19c7a81905897a71f348
darkduy <hung0978682878@gmail.com>
2025-08-07 20:16:29 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
-// jest.config.js
 module.exports = {
   roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  
-  // THÊM MỤC NÀY ĐỂ SỬA LỖI:
-  // Đảm bảo ts-jest xử lý tất cả các tệp .ts/.tsx mà nó gặp phải
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
